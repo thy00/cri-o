031c8f710a2a37dedc93a49647126e385d42ea09
thy00 <floatthy@163.com>
2024-05-16 12:51:31 +0800

    update nix builder

--- nix/default-arm64.nix
+++ nix/default-arm64.nix
@@ -1,24 +1,33 @@
 let
   static = import ./static.nix;
 
-  pkgs = (import ./nixpkgs.nix {
-    crossSystem = {
-      config = "aarch64-unknown-linux-gnu";
+  # 加载 nixpkgs 文件配置，这里需要引入系统参数
+  json = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+  nixpkgs = import (builtins.fetchTarball {
+    name = "nixos-unstable-arm64";
+    url = "${json.url}/archive/${json.rev}.tar.gz";
+    inherit (json) sha256;
+  }) {
+    system = "aarch64-linux"; 
+  };
+
+  pkgs = nixpkgs // {
+    go = nixpkgs.go.overrideAttrs (oldAttrs: {
+      doCheck = false; # 关闭 Go 包的检查阶段
+    });
+    gpgme = (static nixpkgs.gpgme);
+    libassuan = (static nixpkgs.libassuan);
+    libgpgerror = (static nixpkgs.libgpgerror);
+    libseccomp = (static nixpkgs.libseccomp);
+    gnupg = nixpkgs.gnupg.override {
+      libusb1 = null;
+      pcsclite = null;
     };
-    config = {
-      packageOverrides = pkg: {
-        gpgme = (static pkg.gpgme);
-        libassuan = (static pkg.libassuan);
-        libgpgerror = (static pkg.libgpgerror);
-        libseccomp = (static pkg.libseccomp);
-        gnupg = pkg.gnupg.override {
-          libusb1 = null;
-          pcsclite = null;
-        };
-      };
-    };
-  });
+  };
 
   self = import ./derivation.nix { inherit pkgs; };
 in
-self
+self // {
+  # Debug output for verification in Nix REPL
+  goDoCheck = nixpkgs.lib.traceVal pkgs.go.doCheck;
+}
--- nix/default.nix
+++ nix/default.nix
@@ -1,23 +1,32 @@
+{ system ? builtins.currentSystem }:
+
 let
   static = import ./static.nix;
-  pkgs = (import ./nixpkgs.nix {
-    config = {
-      packageOverrides = pkg: {
-        gpgme = (static pkg.gpgme);
-        libassuan = (static pkg.libassuan);
-        libgpgerror = (static pkg.libgpgerror);
-        libseccomp = (static pkg.libseccomp);
-        gnupg = pkg.gnupg.override {
-          libusb1 = null;
-          pcsclite = null;
-        };
-        go = pkgs.go.overrideAttrs (oldAttrs: {
-          doCheck = false;
-        });
-      };
+  json = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+  nixpkgs = import (builtins.fetchTarball {
+    name = "nixos-unstable";
+    url = "${json.url}/archive/${json.rev}.tar.gz";
+    inherit (json) sha256;
+  }) { inherit system; };
+
+  pkgs = nixpkgs // {
+    go = nixpkgs.go.overrideAttrs (oldAttrs: {
+      doCheck = false; # Overriding go package to disable checks
+    });
+    # Apply static as described in static.nix
+    gpgme = (static nixpkgs.gpgme);
+    libassuan = (static nixpkgs.libassuan);
+    libgpgerror = (static nixpkgs.libgpgerror);
+    libseccomp = (static nixpkgs.libseccomp);
+    gnupg = nixpkgs.gnupg.override {
+      libusb1 = null;
+      pcsclite = null;
     };
-  });
+  };
 
   self = import ./derivation.nix { inherit pkgs; };
 in
-self
+self // {
+  # Debug output for verification in Nix REPL
+  goDoCheck = nixpkgs.lib.traceVal pkgs.go.doCheck;
+}
